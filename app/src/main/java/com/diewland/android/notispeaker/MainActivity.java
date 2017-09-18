a81ae3deccec47ab147494a3e5eeb08c8ec2c19e
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-09-18 17:27:51 +0700

    add sms listener, big font size.

--- app/src/main/java/com/diewland/android/notispeaker/MainActivity.java
+++ app/src/main/java/com/diewland/android/notispeaker/MainActivity.java
@@ -1,70 +1,91 @@
 package com.diewland.android.notispeaker;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TableLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TableLayout tab;
-    private EditText text;
-    private Button send;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+    private String list_text;
+
+    public static final String PREFERENCES_TODO = "TODO_List_Shared_Preferences";
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //speakTH("คุณมี 1 ข้อความใหม่");
-        //speakEN("You got new message");
+        arrayList = new ArrayList<String>();
+        list = (ListView) findViewById(R.id.list);
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.mylist, arrayList);
+        list.setAdapter(adapter);
 
-        tab = (TableLayout)findViewById(R.id.tab);
-        text = (EditText)findViewById(R.id.text);
-        send = (Button)findViewById(R.id.send);
+        // Get intent object sent from the SMSBroadcastReceiver
+        Intent sms_intent=getIntent();
+        Bundle b=sms_intent.getExtras();
+        if(b!=null){
+            String s = b.getString("sms_sender");
+            String m = b.getString("sms_message");
+            String t = b.getString("sms_time");
 
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showNotification(text.getText().toString());
-            }
-        });
+            list_text = t + " " + m;
+
+            list.post(new Runnable() {
+                @Override
+                public void run() {
+                    arrayList.add(0, list_text);
+                    adapter.notifyDataSetChanged();
+                    list.smoothScrollToPosition(0);
+                }
+            });
+
+            speak(m);
+        }
+
+        // keep listview state
+        prefs =  getSharedPreferences(PREFERENCES_TODO, MODE_PRIVATE);
+        editor = prefs.edit();
     }
 
-    // https://alvinalexander.com/android/how-to-create-android-notifications-notificationmanager-examples
-    public void showNotification(String s){
-        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
-        Notification notification = new NotificationCompat.Builder(this)
-            .setTicker("Demo")
-            .setSmallIcon(android.R.drawable.ic_menu_help)
-            .setContentTitle("Demo Notification")
-            .setContentText(s)
-            .setContentIntent(pi)
-            .setAutoCancel(true)
-            .build();
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification);
-    }
-
-    private void speakTH(String text){
+    private void speak(String text){
         MyTTS.getInstance(getApplicationContext())
              .setLocale(new Locale("th"))
              .speak(text);
     }
 
-    private void speakEN(String text){
-        MyTTS.getInstance(getApplicationContext())
-                .setLocale(Locale.ENGLISH)
-                .speak(text);
+    @Override
+    protected void onPause() {
+        editor.clear();
+        for (int i = 0; i < adapter.getCount(); ++i){
+            editor.putString(String.valueOf(i), adapter.getItem(i));
+        }
+        editor.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        for (int i = 0;; ++i){
+            final String str = prefs.getString(i+"", "");
+            if (!str.equals("")){
+                adapter.add(str);
+            } else {
+                break; // Empty String means the default value was returned.
+            }
+        }
     }
 }
